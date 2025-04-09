78d1270a6490e1e4e7982bf28a3bde6ae555fa7d
AALUND13 <tamlund1976@gmail.com>
2025-04-09 18:18:11 -0400

    Remove the logging enable check for Warning and Error

--- Scripts/Utils/LoggerUtils.cs
+++ Scripts/Utils/LoggerUtils.cs
@@ -5,7 +5,7 @@ namespace AALUND13Card {
         public static bool logging = ConfigHandler.DebugMode.Value;
 
         public static void LogInfo(string message) { if(logging) AALUND13_Cards.ModLogger.LogInfo(message); }
-        public static void LogWarn(string message) { if(logging) AALUND13_Cards.ModLogger.LogWarning(message); }
-        public static void LogError(string message) { if(logging) AALUND13_Cards.ModLogger.LogError(message); }
+        public static void LogWarn(string message) { AALUND13_Cards.ModLogger.LogWarning(message); }
+        public static void LogError(string message) { AALUND13_Cards.ModLogger.LogError(message); }
     }
 }
