9492be543096ddf626a50a52a4c15ba5a7599054
AALUND13 <tamlund1976@gmail.com>
2025-08-09 03:44:13 -0400

    Remove `RandomExtension` class

--- Scripts/Extensions/RandomExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace AALUND13Cards.Extensions {
-    public static class RandomExtension {
-        public static float NextFloat(this Random random, float minValue, float maxValue) {
-            return (float)random.NextDouble() * (maxValue - minValue) + minValue;
-        }
-    }
-}
