b83bfb423e2a86cc4d902331f80b1dc6cdf298a8
AALUND13 <tamlund1976@gmail.com>
2026-01-25 00:14:32 -0500

    Made store damage from `Dreadful Burst` persist between rounds

--- Classes/Scripts/MonoBehaviours/CardsEffects/Soulstreak/DreadfulBurstEffect.cs
+++ Classes/Scripts/MonoBehaviours/CardsEffects/Soulstreak/DreadfulBurstEffect.cs
@@ -3,11 +3,6 @@ using AALUND13Cards.Classes.MonoBehaviours.CardsEffects.Soulstreak.Abilities;
 using AALUND13Cards.Core.Extensions;
 using AALUND13Cards.Core.Handlers;
 using AALUND13Cards.Core.MonoBehaviours;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace AALUND13Cards.Classes.MonoBehaviours.CardsEffects.Soulstreak {
@@ -46,16 +41,11 @@ namespace AALUND13Cards.Classes.MonoBehaviours.CardsEffects.Soulstreak {
             }
         }
 
-        public void OnRevive() {
-            storedDamage = 0f;
-            storedDamage = 0f;
-        }
-        
         public AbilityBarInfo GetBarInfo() {
             return new AbilityBarInfo(
-                true, 
-                MINIMUM_DRAIN_TICKS <= ticks, 
-                ticks, 
+                true,
+                MINIMUM_DRAIN_TICKS <= ticks,
+                ticks,
                 player.data.GetCustomStatsRegistry().GetOrCreate<SoulStreakStats>().DamageStorage
             );
         }
@@ -66,7 +56,10 @@ namespace AALUND13Cards.Classes.MonoBehaviours.CardsEffects.Soulstreak {
         public void OnSoulsAdded(uint addedSouls) { }
 
         public void OnSoulsReset(uint remainingSouls) { }
+        
+        public void OnRevive() { }
 
         public void OnRemove() { }
+
     }
 }
