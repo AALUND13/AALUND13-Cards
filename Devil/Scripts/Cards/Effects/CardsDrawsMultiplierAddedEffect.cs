7ccfa1ee7471527cd2bbd22558c137f86a4c5e1b
AALUND13 <tamlund1976@gmail.com>
2026-02-26 00:25:53 -0500

    Change "Curse of Greed" devil card "3 Cards Draws" to "1/3 Cards Draws"

---
+++ Devil/Scripts/Cards/Effects/CardsDrawsMultiplierAddedEffect.cs
@@ -0,0 +1,18 @@
+﻿using AALUND13Cards.Core;
+using AALUND13Cards.Core.Cards.Effects;
+using UnboundLib;
+using UnityEngine;
+
+namespace AALUND13Cards.Devil.Cards.Effects {
+    public class CardsDrawsMultiplierAddedEffect : OnAddedEffect {
+        [Range(0f, 2f)]
+        public float CardsDrawsMultiplier = 1;
+
+        public override void OnAdded(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers characterStats) {
+            AAC_Core.Instance.ExecuteAfterSeconds(0.5f, () => {
+                int drawToSet = Mathf.RoundToInt(DrawNCards.DrawNCards.GetPickerDraws(player.playerID) * CardsDrawsMultiplier);
+                DrawNCards.DrawNCards.SetPickerDraws(player.playerID, drawToSet);
+            });
+        }
+    }
+}
