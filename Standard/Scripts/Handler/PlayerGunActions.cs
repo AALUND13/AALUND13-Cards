e5b0c923063bec1000c65c9eed1a47982fb769ef
AALUND13 <tamlund1976@gmail.com>
2026-03-05 20:51:53 -0500

    Refactor "TrailBulletChanceEffect" to "ProjectileEffectChanceMono"

---
+++ Standard/Scripts/Handler/PlayerGunActions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AALUND13Cards.Standard.Handler {
+    /// <summary>
+    /// Basically the same as "Gun.ShootPojectileAction" but this will work for ALL guns the player have
+    /// </summary>
+    public class PlayerGunActions {
+        public static Dictionary<Player, Action<Gun, GameObject>> OnGunShootAction = new Dictionary<Player, Action<Gun, GameObject>>();
+
+        public static void AddShootAction(Player player, Action<Gun, GameObject> action) {
+            if(!OnGunShootAction.ContainsKey(player)) OnGunShootAction.Add(player, action);
+            else OnGunShootAction[player] += action;
+        }
+
+        public static void RemoveShootAction(Player player, Action<Gun, GameObject> action) {
+            if(OnGunShootAction.ContainsKey(player)) OnGunShootAction[player] -= action;
+        }
+
+        internal static void InvokeShootAction(Player player, Gun gun, GameObject projectile) {
+            if(!OnGunShootAction.ContainsKey(player)) OnGunShootAction.Add(player, (Action<Gun, GameObject>)null);
+            
+            OnGunShootAction[player]?.Invoke(gun, projectile);
+        }
+    }
+}
---
+++ Standard/Scripts/Patches/GunPatch.cs
@@ -0,0 +1,17 @@
+﻿using AALUND13Cards.Standard.Handler;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AALUND13Cards.Standard.Patches {
+    [HarmonyPatch(typeof(Gun), nameof(Gun.BulletInit))]
+    public class GunPatch {
+        public static void Postfix(Gun __instance, GameObject bullet) {
+            PlayerGunActions.InvokeShootAction(__instance.player, __instance, bullet);
+        }
+    }
+}
