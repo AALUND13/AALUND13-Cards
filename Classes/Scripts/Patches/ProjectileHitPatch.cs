a930ce5c104a53bacc70e3f6a2b740c08866c51c
AALUND13 <tamlund1976@gmail.com>
2026-03-06 22:52:23 -0500

    Fixed "Bleed" effect not sync on all clients

--- Classes/Scripts/Patches/ProjectileHitPatch.cs
+++ Classes/Scripts/Patches/ProjectileHitPatch.cs
@@ -3,17 +3,33 @@ using AALUND13Cards.Classes.Cards;
 using AALUND13Cards.Core.Extensions;
 using HarmonyLib;
 using JARL.Armor;
+using Photon.Pun;
 using UnboundLib;
 using UnityEngine;
 
 namespace AALUND13Cards.Classes.Patches {
-    [HarmonyPatch(typeof(ProjectileHit), "Hit")]
+    [HarmonyPatch(typeof(ProjectileHit), "RPCA_DoHit")]
     internal class ProjectileHitPatch {
-        private static bool Prefix(ProjectileHit __instance, HitInfo hit, bool forceCall) {
-            HealthHandler healthHandler = null;
-            if(hit.transform) {
-                healthHandler = hit.transform.GetComponent<HealthHandler>();
+        private static bool Prefix(ProjectileHit __instance, Vector2 hitPoint, Vector2 hitNormal, int viewID = -1, int colliderID = -1, bool wasBlocked = false) {
+            HitInfo hitInfo = new HitInfo();
+
+            hitInfo.point = hitPoint;
+            hitInfo.normal = hitNormal;
+            hitInfo.collider = null;
+            if(viewID != -1) {
+                PhotonView photonView = PhotonNetwork.GetPhotonView(viewID);
+                hitInfo.collider = photonView.GetComponentInChildren<Collider2D>();
+                hitInfo.transform = photonView.transform;
+            } else if(colliderID != -1) {
+                hitInfo.collider = MapManager.instance.currentMap.Map.GetComponentsInChildren<Collider2D>()[colliderID];
+                hitInfo.transform = hitInfo.collider.transform;
             }
+
+            HealthHandler healthHandler = null;
+            if((bool)hitInfo.transform) {
+                healthHandler = hitInfo.transform.GetComponent<HealthHandler>();
+            }
+
             if(healthHandler) {
                 Player hitPlayer = healthHandler.GetComponent<Player>();
 
@@ -22,7 +38,7 @@ namespace AALUND13Cards.Classes.Patches {
                     float percentageDamage = GetBulletDamage(__instance, hitPlayer);
                     hitPlayer.data.healthHandler.TakeDamageOverTime(
                         __instance.transform.forward * percentageDamage * __instance.ownPlayer.data.GetCustomStatsRegistry().GetOrCreate<ReaperStats>().PercentageDamageBleedingPercentage,
-                        hit.point, 5, 1f, Color.red * 0.8f, __instance.ownWeapon, __instance.ownPlayer, true
+                        hitPoint, 5, 1f, Color.red * 0.8f, __instance.ownWeapon, __instance.ownPlayer, true
                     );
                 }
             }
