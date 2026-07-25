02f3754a7435f6336f9f3f47b3a4a31ac2ff304a
AALUND13 <58748165+AALUND13@users.noreply.github.com>
2026-07-24 17:33:46 -0700

    Refactor LoggerUtils with LogLevel and caller info

--- Core/Scripts/Utils/LoggerUtils.cs
+++ Core/Scripts/Utils/LoggerUtils.cs
@@ -1,9 +1,31 @@
-﻿namespace AALUND13Cards.Core {
-    public static class LoggerUtils {
-        public static bool logging = AACMenu.DebugMode.Value;
+﻿using BepInEx.Logging;
+using System.Diagnostics;
 
-        public static void LogInfo(string message, bool bypassCheck = false) { if(logging || bypassCheck) AAC_Core.ModLogger.LogInfo(message); }
-        public static void LogWarn(string message) { AAC_Core.ModLogger.LogWarning(message); }
-        public static void LogError(string message) { AAC_Core.ModLogger.LogError(message); }
+namespace AALUND13Cards.Core {
+    public static class LoggerUtils {
+#if DEBUG
+        const bool DEBUG = true;
+#else
+        const bool DEBUG = false;
+#endif
+
+        private static void Loginternal(LogLevel level, string message, bool bypassCheck = false) {
+            if (LogLevel.Debug > level || DEBUG || bypassCheck) {
+                var callerFrame = new StackFrame(2);
+                var callerMethod = callerFrame.GetMethod();
+                string className = callerMethod.DeclaringType.Name;
+
+                AAC_Core.ModLogger.Log(level, $"[{className}] {message}");
+            }
+        }
+
+        public static void Log(LogLevel level, string message, bool bypassCheck = false) => 
+            Loginternal(level, message, bypassCheck);
+        public static void LogInfo(string message, bool bypassCheck = false) => 
+            Loginternal(LogLevel.Info, message, bypassCheck);
+        public static void LogWarn(string message) => 
+            Loginternal(LogLevel.Warning, message, true);
+        public static void LogError(string message) => 
+            Loginternal(LogLevel.Error, message, true);
     }
 }
